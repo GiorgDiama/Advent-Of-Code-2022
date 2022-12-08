f4a1859b24de53d26178ecf6fc9a025986647541
GiorDiama <giorg.diama@gmail.com>
2022-12-08 15:36:26 +0000

    comments

--- d7/logic.py
+++ d7/logic.py
@@ -4,18 +4,20 @@ class Tree:
         self.nodes = []
 
     def add_node(self, path, node):
-        path = path.split('.')
+        path = path.split('.') # get names of dirs in the path
 
-        parent = self.root
-        parent.size += node.size
+        parent = self.root # starting on root
 
+        parent.size += node.size # add size to every parent
+        # traverse the tree to find parent
         for p in path[1:]:
             for c in parent.children:
                 if c.name == p:
                     parent = c
                     break
-            parent.size += node.size
 
+            parent.size += node.size # add size to every parent
+        # add node to tree and to a list of all dirs
         node.parent = parent
         parent.children.append(node)
         self.nodes.append(node)
@@ -38,6 +40,7 @@ class Node:
         return f"{self.name} | {self.size} | {self.type}"
 
     def disp_subtree(self, spaces=0):
+        # recursivly print a sub_tree starting on this node
         print("   "*spaces, self.__str__())
         for c in self.children:
             c.disp_subtree(spaces+1)
@@ -47,7 +50,7 @@ def build_tree():
     data = map(lambda x:x[:-1].split(), open('input.txt'))
 
     t = Tree()
-
+    # follow the commands storingthe current path and create the tree
     path = []
     for line in data:
         if line[1] == "cd":
